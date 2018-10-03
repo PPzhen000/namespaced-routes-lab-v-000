bc7b4e59add660a69a8072900fc6979ab0fff68b
Ariel Zhen <zhenpingping@live.com>
2018-10-03 07:01:50 +0000

    automatically backed up by learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,3 +1,19 @@
 class Preference < ActiveRecord::Base
 
+  def song_sort_order
+
+  end 
+
+  def artist_sort_order
+
+  end 
+
+  def allow_create_artists
+
+  end 
+
+  def allow_create_songs
+
+  end 
+  
 end
